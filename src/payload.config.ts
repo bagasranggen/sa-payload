f3d7433934d4ce252cc12c0bc5893ff93db76380
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-02-26 15:58:06 +0700

    refactor: setup s3 storage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,34 +1,54 @@
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
-import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
-import sharp from 'sharp'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import sharp from 'sharp';
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
+import { buildConfig } from 'payload';
+import { postgresAdapter } from '@payloadcms/db-postgres';
+import { lexicalEditor } from '@payloadcms/richtext-lexical';
+import { s3Storage } from '@payloadcms/storage-s3';
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
+import { Users } from './collections/Users';
+import { Media } from './collections/Media';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default buildConfig({
-  admin: {
-    user: Users.slug,
-    importMap: {
-      baseDir: path.resolve(dirname),
+    admin: {
+        user: Users.slug,
+        importMap: {
+            baseDir: path.resolve(dirname),
+        },
     },
-  },
-  collections: [Users, Media],
-  editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
-  typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
-  },
-  db: postgresAdapter({
-    pool: {
-      connectionString: process.env.DATABASE_URL || '',
+    collections: [Users, Media],
+    editor: lexicalEditor(),
+    secret: process.env.PAYLOAD_SECRET || '',
+    typescript: {
+        outputFile: path.resolve(dirname, 'payload-types.ts'),
     },
-  }),
-  sharp,
-  plugins: [],
-})
+    db: postgresAdapter({
+        pool: {
+            connectionString: process.env.DATABASE_URL || '',
+        },
+    }),
+    sharp,
+    plugins: [
+        s3Storage({
+            collections: {
+                media: {
+                    prefix: 'media',
+                },
+            },
+            bucket: process.env.S3_BUCKET,
+            config: {
+                forcePathStyle: true,
+                credentials: {
+                    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+                    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+                },
+                region: process.env.S3_REGION,
+                endpoint: process.env.S3_ENDPOINT,
+            },
+        }),
+    ],
+});
