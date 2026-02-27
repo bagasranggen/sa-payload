b337686ec6be155f101bafc0dd90a411644f835b
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-02-27 11:03:40 +0700

    refactor: create shared fields for assets

---
+++ src/collections/shared/BaseAssets.ts
@@ -0,0 +1,37 @@
+import { CollectionConfig, UploadConfig } from 'payload';
+
+export type BaseAssetsProps = {} & Pick<CollectionConfig, 'slug'> & Pick<UploadConfig, 'imageSizes'>;
+
+export const BaseAssets = ({ slug, imageSizes }: BaseAssetsProps): CollectionConfig => {
+    return {
+        slug,
+        admin: {
+            group: 'Assets',
+        },
+        access: {
+            read: (arg) => {
+                const {
+                    req: { headers },
+                } = arg;
+
+                const hostnames = [process.env.CMS_HOSTNAME].filter((item) => Boolean(item));
+                const forwardedHostname = headers?.get('x-forwarded-host')?.split(':').shift();
+
+                return hostnames.includes(forwardedHostname);
+            },
+        },
+        fields: [
+            {
+                name: 'alt',
+                type: 'text',
+                required: true,
+            },
+        ],
+        upload: {
+            // staticDir: 'media',
+            // skipSafeFetch: [{ hostname: process.env.CMS_HOSTNAME || '' }],
+            disableLocalStorage: true,
+            imageSizes,
+        },
+    };
+};
