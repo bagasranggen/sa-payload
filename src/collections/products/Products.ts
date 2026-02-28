536288f37b9b97e27e221100a381428e4555ed77
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-02-28 19:00:34 +0700

    refactor: update product collection

--- src/collections/products/Products.ts
+++ src/collections/products/Products.ts
@@ -35,8 +35,125 @@ export const Products: CollectionConfig = {
                 fields: [],
             },
             {
+                label: 'Summaries',
+                fields: [
+                    {
+                        type: 'array',
+                        name: 'summaries',
+                        interfaceName: 'Summaries',
+                        label: false,
+                        labels: {
+                            plural: 'Summaries',
+                            singular: 'Summary',
+                        },
+                        fields: [
+                            {
+                                type: 'text',
+                                name: 'title',
+                            },
+                            {
+                                type: 'array',
+                                name: 'details',
+                                fields: [
+                                    {
+                                        type: 'row',
+                                        fields: [
+                                            {
+                                                type: 'relationship',
+                                                relationTo: 'labels',
+                                                name: 'label',
+                                                admin: {
+                                                    width: '50%',
+                                                },
+                                            },
+                                            {
+                                                type: 'text',
+                                                name: 'value',
+                                                admin: {
+                                                    width: '50%',
+                                                },
+                                            },
+                                        ],
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                label: 'Booked Dates',
+                fields: [
+                    {
+                        type: 'array',
+                        name: 'bookedDates',
+                        label: false,
+                        labels: {
+                            plural: 'Booked Date',
+                            singular: 'Booked Date',
+                        },
+                        fields: [
+                            {
+                                type: 'row',
+                                fields: [
+                                    {
+                                        type: 'date',
+                                        name: 'from',
+                                    },
+                                    {
+                                        type: 'date',
+                                        name: 'to',
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
                 label: 'Content',
                 fields: [
+                    {
+                        type: 'group',
+                        fields: [
+                            {
+                                type: 'array',
+                                name: 'prices',
+                                interfaceName: 'Prices',
+                                fields: [
+                                    {
+                                        type: 'row',
+                                        fields: [
+                                            {
+                                                type: 'number',
+                                                name: 'price',
+                                                required: true,
+                                                admin: {
+                                                    width: '44%',
+                                                },
+                                            },
+                                            {
+                                                type: 'number',
+                                                name: 'salePrice',
+                                                admin: {
+                                                    width: '44%',
+                                                },
+                                            },
+                                            {
+                                                type: 'number',
+                                                name: 'days',
+                                                defaultValue: 3,
+                                                required: true,
+                                                admin: {
+                                                    width: '12%',
+                                                },
+                                            },
+                                        ],
+                                    },
+                                ],
+                            },
+                        ],
+                    },
                     {
                         type: 'row',
                         fields: [
@@ -50,9 +167,26 @@ export const Products: CollectionConfig = {
                             },
                             {
                                 type: 'relationship',
+                                name: 'colors',
+                                relationTo: 'colors',
+                                hasMany: true,
+                                admin: {
+                                    width: '50%',
+                                },
+                            },
+                            {
+                                type: 'relationship',
                                 name: 'sizes',
                                 relationTo: 'sizes',
                                 hasMany: true,
+                                admin: {
+                                    width: '50%',
+                                },
+                            },
+                            {
+                                type: 'relationship',
+                                name: 'tag',
+                                relationTo: 'tags',
                                 admin: {
                                     width: '50%',
                                 },
