b1df1edf34cdefacbbb69b472c837defcee20f31
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-03-03 00:04:32 +0700

    refactor: update cors config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -16,6 +16,7 @@ const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 export default buildConfig({
+    cors: ['http://dev.sa'],
     admin: {
         user: Users.slug,
         importMap: {
