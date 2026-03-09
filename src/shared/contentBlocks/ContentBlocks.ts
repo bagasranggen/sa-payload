9fed2ff6b6e62954b62a2e9e794b59decc10780c
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-03-09 16:20:25 +0700

    refactor: update content block field

--- src/shared/contentBlocks/ContentBlocks.ts
+++ src/shared/contentBlocks/ContentBlocks.ts
@@ -2,16 +2,20 @@ import { BlocksField, Field } from 'payload';
 
 import { ContentBlocksLabelContent } from '@/shared/contentBlocks/ContentBlocksLabelContent';
 
-export type ContentBlocksProps = {} & Pick<BlocksField, 'name'>;
+export type ContentBlocksProps = Pick<BlocksField, 'name'>;
 
 export const ContentBlocks = (props?: ContentBlocksProps): Field => {
     return {
-        type: 'blocks',
+        type: 'group',
+        label: '',
         name: props?.name ?? 'contentBlocks',
-        // interfaceName: 'ContentBlock',
-        // fields: [
-        //
-        // ],
-        blocks: [ContentBlocksLabelContent],
+        interfaceName: 'ContentBlocks',
+        fields: [
+            {
+                type: 'blocks',
+                name: 'blocks',
+                blocks: [ContentBlocksLabelContent],
+            },
+        ],
     };
 };
--- src/shared/contentBlocks/ContentBlocksLabelContent.ts
+++ src/shared/contentBlocks/ContentBlocksLabelContent.ts
@@ -1,17 +1,26 @@
 import { Block } from 'payload';
 
+import { BaseContentBlocksField } from '@/shared/contentBlocks/BaseContentBlocksField';
+
 export const ContentBlocksLabelContent: Block = {
     slug: 'labelContent',
-    interfaceName: 'ContentBlockLabelContent',
-    fields: [
-        {
-            type: 'text',
-            name: 'heading',
-            required: true,
-        },
-        {
-            type: 'richText',
-            name: 'content',
-        },
-    ],
+    interfaceName: 'CbLabelContent',
+    fields: BaseContentBlocksField({
+        tabs: [
+            {
+                label: 'Content',
+                fields: [
+                    {
+                        type: 'text',
+                        name: 'heading',
+                        required: true,
+                    },
+                    {
+                        type: 'richText',
+                        name: 'content',
+                    },
+                ],
+            },
+        ],
+    }),
 };
