fdee272859bd69b6bf30ae19c29472dca7ddc7ab
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-03-15 18:05:46 +0700

    refactor: update product media field

--- src/collections/pages/Products.ts
+++ src/collections/pages/Products.ts
@@ -38,6 +38,8 @@ export const Products: CollectionConfig = {
                         type: 'upload',
                         name: 'media',
                         relationTo: 'mediaProducts',
+                        hasMany: true,
+                        required: true,
                     },
                 ],
             },
