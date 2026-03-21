12bb27e7371eddb23f4354d0fe114d62a3dcb2e3
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-03-21 10:22:20 +0700

    refactor: add image size transform

--- src/collections/assets/Media.ts
+++ src/collections/assets/Media.ts
@@ -4,4 +4,11 @@ import { BaseAssets } from '@/shared';
 
 export const Media: CollectionConfig = BaseAssets({
     slug: 'media',
+    imageSizes: [
+        {
+            name: 'assets800x800',
+            width: 800,
+            height: 800,
+        },
+    ],
 });
