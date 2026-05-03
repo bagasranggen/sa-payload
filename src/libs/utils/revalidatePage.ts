825309e5d13d55d8fcb47e2542da8f8201f87425
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-05-03 09:47:09 +0700

    refactor: update revalidation libs

--- src/libs/utils/revalidatePage.ts
+++ src/libs/utils/revalidatePage.ts
@@ -1,13 +1,17 @@
 import { ArrayStringTypes } from '@/libs/types';
-import { joinArrayString } from './joinArrayString';
+import { joinArrayString } from '@/libs/utils/joinArrayString';
 
-export type RevalidatePageType = {
+export type RevalidatePageItemProps = {
     path: string;
     layout?: 'page' | 'layout';
     hasDynamicSegment?: boolean;
 };
 
-export const revalidatePage = async ({ path, layout = 'page', hasDynamicSegment }: RevalidatePageType) => {
+export type RevalidatePageProps = {
+    items: RevalidatePageItemProps[];
+};
+
+export const revalidatePage = async ({ items }: RevalidatePageProps) => {
     if (!process.env.BASE_URI) return;
     if (!process.env.REVALIDATION_SECRET_TOKEN) return;
 
@@ -16,16 +20,21 @@ export const revalidatePage = async ({ path, layout = 'page', hasDynamicSegment 
     baseUrl.push('revalidate');
     baseUrl = joinArrayString(baseUrl, '/');
 
-    let params: ArrayStringTypes = [`secret=${process.env.REVALIDATION_SECRET_TOKEN}`];
-    params.push(`path=${path}`);
-    if (hasDynamicSegment || layout === 'layout') params.push(`type=${layout}`);
-    params = joinArrayString(params, '&');
+    if (items && items.length > 0) {
+        for (const item of items) {
+            let params: ArrayStringTypes = [`secret=${process.env.REVALIDATION_SECRET_TOKEN}`];
+            params.push(`path=${item.path}`);
+            if (item?.hasDynamicSegment || item?.layout === 'layout') params.push(`type=${item.layout}`);
+            params = joinArrayString(params, '&');
 
-    const url = joinArrayString([baseUrl, params], '?');
+            const url = joinArrayString([baseUrl, params], '?');
 
-    try {
-        await fetch(url);
-    } catch (e) {
-        console.error(e);
+            try {
+                await fetch(url);
+                console.log({ message: 'run', url });
+            } catch (e) {
+                console.error(e);
+            }
+        }
     }
 };
