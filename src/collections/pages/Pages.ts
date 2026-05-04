919da329037b09ddd823460d89eb0d7655510507
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-05-04 11:23:07 +0700

    refactor: integrate after change hook on pages

--- src/collections/pages/Pages.ts
+++ src/collections/pages/Pages.ts
@@ -1,8 +1,9 @@
 import { CollectionConfig } from 'payload';
 
-import { PAGES_TYPE_HANDLES, PAGES_TYPE_OPTIONS_HANDLES } from '@/libs/constants';
-
 import { BaseEntry, ContentBlocks } from '@/shared';
+
+import { PAGES_TYPE_HANDLES, PAGES_TYPE_OPTIONS_HANDLES } from '@/libs/constants';
+import { revalidatePage, RevalidatePageProps } from '@/libs/utils';
 
 export const Pages: CollectionConfig = {
     slug: 'pages',
@@ -11,6 +12,49 @@ export const Pages: CollectionConfig = {
         useAsTitle: 'title',
         groupBy: true,
     },
+    hooks: {
+        afterChange: [
+            async ({ doc, req: { payload } }) => {
+                console.log({ doc });
+                const typeHandle = doc.typeHandle;
+                const revalidatePaths: RevalidatePageProps['items'] = [];
+
+                if (doc?.uri) revalidatePaths.push({ path: `/${doc?.uri}` });
+
+                try {
+                    if (typeHandle === PAGES_TYPE_OPTIONS_HANDLES[PAGES_TYPE_HANDLES.PRODUCTS_CATEGORIES]) {
+                        const homepage = await payload.findGlobal({
+                            slug: 'homepage',
+                        });
+
+                        const collectionIDs: number[] = [];
+                        if (homepage && homepage?.collections && homepage?.collections.length > 0) {
+                            homepage.collections.forEach((item) => {
+                                const category = item && typeof item !== 'number' ? item?.productCategory : undefined;
+
+                                if (
+                                    typeof item !== 'number' &&
+                                    category &&
+                                    typeof category !== 'number' &&
+                                    category?.id &&
+                                    item?.id
+                                ) {
+                                    collectionIDs.push(category.id);
+                                }
+                            });
+                        }
+
+                        if (collectionIDs.includes(doc?.productCategory)) revalidatePaths.push({ path: '/' });
+                    }
+                } catch (e) {
+                    console.log(e);
+                }
+
+                await revalidatePage({ items: revalidatePaths });
+            },
+        ],
+    },
+
     fields: BaseEntry({
         typeHandle: [
             PAGES_TYPE_OPTIONS_HANDLES[PAGES_TYPE_HANDLES.ORDERS],
@@ -23,7 +67,6 @@ export const Pages: CollectionConfig = {
                 const typeHandle = siblingData?.typeHandle;
 
                 if (typeHandle === PAGES_TYPE_HANDLES.PRODUCTS_CATEGORIES) return false;
-                // console.log({ from: 'slug', siblingData });
 
                 return true;
             },
