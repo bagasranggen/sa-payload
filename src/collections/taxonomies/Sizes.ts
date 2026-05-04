a857ff31753c2a42994ac031d339d1ebc7b6f252
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-05-04 13:43:14 +0700

    refactor: ad libs for taxonomies

--- src/collections/taxonomies/Sizes.ts
+++ src/collections/taxonomies/Sizes.ts
@@ -2,11 +2,26 @@ import { CollectionConfig } from 'payload';
 
 import { BaseEntry } from '@/shared';
 
+import { revalidatePage, RevalidatePageProps } from '@/libs/utils';
+import { getCollectionUrl } from '@/libs/factory';
+
 export const Sizes: CollectionConfig = {
     slug: 'sizes',
     admin: {
         group: 'Taxonomies',
         useAsTitle: 'title',
+    },
+    hooks: {
+        afterChange: [
+            async ({ req: { payload } }) => {
+                const revalidatePaths: RevalidatePageProps['items'] = [{ path: '/collection' }];
+
+                const paths = await getCollectionUrl({ payload });
+                if (paths) revalidatePaths.push(...paths);
+
+                await revalidatePage({ items: revalidatePaths });
+            },
+        ],
     },
     fields: BaseEntry({
         typeHandle: 'sectionTaxonomiesSizes',
